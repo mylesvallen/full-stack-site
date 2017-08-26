5bec6359f22e5ae149fecdaef17476dc78c7cc5f
cherisek27 <cheriseknox27@gmail.com>
2017-08-26 10:42:35 -0700

    update

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 
-<<<<<<< HEAD
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -14,11 +13,11 @@ var PORT = process.env.PORT || 3000;
 
 
 var db = require("./models");
-=======
+
 //assigned var 'PORT' to a hosting service env
 //or local host 3000
 var PORT = process.env.PORT || 3000;
->>>>>>> origin
+
 
 //this middleware is used to serve everything from 
 //our 'public' directory (html,css,js,imgs)
@@ -31,7 +30,7 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 =======
 // var routes = require("./routes/tenantLogin.js");
->>>>>>> origin
+
 
 
 //will be use for handlebars
@@ -39,16 +38,15 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-<<<<<<< HEAD
-=======
+
 var tenantLogin = require("./controllers/tenantlogin_controller.js");
 app.use(tenantLogin);
->>>>>>> origin
+
 
 var propertyRatings = require("./controllers/propratings_controller.js");
 app.use(propertyRatings);
 
-<<<<<<< HEAD
+
 
 db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
@@ -56,10 +54,9 @@ db.sequelize.sync().then(function() {
   });
 });
 
-=======
 //we call listen() 
 //which binds our server to PORT  
 app.listen(PORT, function() {
     console.log("Listening on PORT: ", PORT);
 });
->>>>>>> origin
+
